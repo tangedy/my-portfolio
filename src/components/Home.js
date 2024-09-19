3da8d7f30627144b5abb05d3e9f974920962d44e
Edward Tang <edtangingson@gmail.com>
2024-09-19 00:14:50 -0400

    edit home msg and formatting

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <section id="home" style={{ backgroundColor: '#121212', color: '#fff', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    <h1>I'm flattered you've taken an interest in me, have a look around ~</h1>
+    <h1>home message: hello hello hi!!! Im Eddie!!!</h1>
   </section>
   );
 };
