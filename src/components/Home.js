65b2747553fe18e5234786bff3ea21805e8321d1
Edward Tang <edtangingson@gmail.com>
2024-09-19 01:08:12 -0400

    tryig newlines

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <section id="home" style={{ backgroundColor: '#121212', color: '#fff', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    <h1>home message: hello hello hi!!! Im Eddie!!!</h1>
+    <h1>home message: hello\
+      hello hi!!!\
+       Im Eddie!!!</h1>
   </section>
   );
 };
