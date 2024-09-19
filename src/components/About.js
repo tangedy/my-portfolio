7b46e5120668d67c8ef18470675823f01ac266f9
Edward Tang <edtangingson@gmail.com>
2024-09-18 22:30:30 -0400

    edit app.css for aesthetics

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id= "about" style={{ backgroundColor: '#1e1e1e', color: '#fff', padding: '50px' }}>
       <h2>A Bit About Me</h2>
-      <p> Im Eddie, your charming honours student at the University of Waterloo studying mathematics. 
+      <p> Im Eddie, your charming honours student at the University of Waterloo studying mathematics! 
         
         with a passion for math, computer science, data science, and artificial intelligence. 
         
