9a90af6ee7fed285e148f780434f88062a9bc8bf
Edward Tang <edtangingson@gmail.com>
2024-09-18 22:34:48 -0400

    edit contact email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section id= "contact" style={{ backgroundColor: '#1e1e1e', color: '#fff', padding: '50px' }}>
       <h2>Contact</h2>
-      <p>Get in touch with me at: example@example.com</p>
+      <p>Get in touch with me at: edward.tang0905@gmail.com</p>
     </section>
   );
 };
