7dcd156cb7e2b82b2cf928e949fe2735e5431966
Edward Tang <edtangingson@gmail.com>
2024-09-20 20:31:32 -0400

    delete css files and prime home.js for css

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <section id="home" style={{ backgroundColor: '#121212', color: '#fff', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <section id="home">
     <h1>home message: hello\
       hello hi!!!\
        Im Eddie!!!</h1>
