fff4f87f269891fa8df497a8b517347607f03be9
Edward Tang <edtangingson@gmail.com>
2024-09-20 20:49:24 -0400

    navbar colours and homemsg

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <section id="home">
-    <h1>home message: hello\
-      hello hi!!!\
-       Im Eddie!!!</h1>
+    <h1>home \n hello</h1>
   </section>
   );
 };
