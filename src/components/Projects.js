0bbd92dddd6de0bda05a3533b6ad0db366190501
Edward Tang <edtangingson@gmail.com>
2024-09-21 22:53:07 -0400

    this totally breaks my code lol

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Projects.css';
-import recipeRadarImg from 'src/assets/recipe-radar.png';
+import recipeRadarImg from 'recipe-radar.png';
 
 const Projects = () => {
   return (
