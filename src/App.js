5cd175cd526242e91ffdbc95b32b65243af67032
Edward Tang <edtangingson@gmail.com>
2024-09-21 23:25:14 -0400

    i dont even know anymore

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <img src ={signatureImg} alt = 'Signature' className="Signature" />
+
       <Navbar />
       <Home />
       <About />
