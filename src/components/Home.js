973ed09588138c85a7c5a818f26bfc9038cf2e79
Edward Tang <edtangingson@gmail.com>
2024-09-22 00:41:40 -0400

    home message

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <section id="home">
-    <h1>home + hello</h1>
+    <h1>Hello</h1>
+    <h2>I'm flattered you've taken an interest in me.</h2>
+    <h3>Have a look around~</h3>
   </section>
   );
 };
