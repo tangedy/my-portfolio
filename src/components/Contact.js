476aacd4a9c08fb3304e51c09b5dc9b3f9d5aa38
Edward Tang <edtangingson@gmail.com>
2024-09-21 20:20:28 -0400

    contact css update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
     <section id= "contact" style={{ backgroundColor: '#1e1e1e', color: '#fff', padding: '50px' }}>
-      <h2>Contact</h2>
-      <p>Get in touch with me at: edward.tang0905@gmail.com</p>
+      <h2>Contact Me</h2>
+      <p>Feel free to reach out for any collaborations or questions!</p>
+      <div className="contact-links">
+        <a href="edward.tang0905@gmail.com" className="contact-link">Email Me</a>
+        <a href="https://www.linkedin.com/in/edward-tang-98a091311/" className="contact-link">LinkedIn</a>
+      </div>
     </section>
   );
 };
