57ecd02562b9b1ac717c4c1389fc668d97e883e6
Edward Tang <edtangingson@gmail.com>
2024-09-22 19:29:58 -0400

    realign projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ import recipeRadarImg from './recipe-radar.png';
 const Projects = () => {
   return (
     <section id= "projects" style={{ backgroundColor: '#121212', color: '#fff', padding: '50px' }}>
-       <h2>My Projects</h2>
+      <h2>My Projects</h2>
       <div className="projects-container">
         <div className="project-card animate__animated animate__fadeInUp">
 
